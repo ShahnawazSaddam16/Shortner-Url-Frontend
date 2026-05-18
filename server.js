07c7b4a729af9d0b437a0339ab9629195b6c4fa8
ShahanwazSaddam144 <shahnawazsaddamb@gmail.com>
2026-05-19 00:19:50 +0500

    adding cookie parser in main server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const rateLimit = require("express-rate-limit");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const dbConnection = require("./src/utils/dbConnection");
 const UrlControllers = require("./src/controllers/urlControllers");
 const Auth = require("./src/controllers/auth");
@@ -12,6 +13,7 @@ const PORT = process.env.PORT;
 
 //Middleware
 app.use(express.json());
+app.use(cookieParser());
 app.use(
   cors({
     origin: [
